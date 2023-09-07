3e5ddd9ce8d1910976d826f21e6671a2e9c9a8d7
NotBruceC <brucec2026@student.cis.edu.hk>
2023-09-07 14:43:44 +0800

    build

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,28 @@
-public class Dateable {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
+public class Dateable {
+public Dateable(String[] args) {
+Scanner scanner = new Scanner(System.in);
+System.out.println("ENTER your AGE!!");
+int age = scanner.nextInt();
+        while (age<0 ) {
+            try{
+                age = Integer.parseInt(scanner.nextLine());
+            }
+            catch (NumberFormatException e){
+                System.out.println("Please enter a valid age man...");
+            }
+        }
+        System.out.println("Age:" + age);
+       int age2 = age / 2;
+       int agew = age2 + 7;
+       System.out.println("Age of date:" + agew);
+
+
+
+
+
+
         
     }
 }
